63897df1271e2855354d442063fd21d0b915b0fe
laserguy22 <coolmichael26@gmail.com>
2020-06-28 15:22:40 -0400

    added all screens

---
+++ app/src/main/java/com/example/michaelarkhangelskiy_final/ui/saved/SavedViewModel.java
@@ -0,0 +1,18 @@
+package com.example.michaelarkhangelskiy_final.ui.saved;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SavedViewModel extends ViewModel {
+    private MutableLiveData<String> mText;
+
+    public SavedViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is saved fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/com/example/michaelarkhangelskiy_final/ui/settings/settings.java
@@ -0,0 +1,39 @@
+package com.example.michaelarkhangelskiy_final.ui.settings;
+
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.michaelarkhangelskiy_final.R;
+import com.example.michaelarkhangelskiy_final.ui.notifications.NotificationsViewModel;
+
+public class settings extends Fragment {
+
+    private SettingsViewModel settingsViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        settingsViewModel =
+                ViewModelProviders.of(this).get(SettingsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_settings, container, false);
+        final TextView textView = root.findViewById(R.id.settings);
+        settingsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+}
